2b2d33cb9abee7f7d89eb275521927e8631b1804
Bradley Kettle <Bradley-k@hotmail.com>
2023-02-24 12:45:11 -0330

    Lab 5 done

--- lab_4/expressLocalLibraryTutorial/express-locallibrary-tutorial/app.js
+++ lab_4/expressLocalLibraryTutorial/express-locallibrary-tutorial/app.js
@@ -1,21 +1,23 @@
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+const mongoDB = "insert_your_database_url_here";
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 var createError = require('http-errors');
 var express = require('express');
+var app = express();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var app = express();
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-const coolRouter = express.Router();
-
-coolRouter.get('/', (req, res) => {
-  res.send('You are so cool');
-});
-
-app.use('/users/cool', coolRouter);
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/users/cool', coolRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
